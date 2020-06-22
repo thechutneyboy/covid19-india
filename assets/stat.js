8ef01e75ad14fe970d25a6b4a60e27870c80abce
thechutneyboy <66558355+thechutneyboy@users.noreply.github.com>
2020-06-22 22:21:01 +0100

    tracker fix with js

---
+++ assets/stat.js
@@ -0,0 +1,10 @@
+var sc_project=12345976;
+var sc_invisible=1;
+var sc_security="f591ed0b";
+var sc_https=1;
+var js = document.createElement("script");
+
+js.type = "text/javascript";
+js.src = "https://www.statcounter.com/counter/counter.js";
+
+document.body.appendChild(js);
